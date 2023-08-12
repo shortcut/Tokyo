c9fff498301ed7b9274b9f8e149f349c355f0ba9
Vikram <vikram.sh86@gmail.com>
2023-08-12 14:32:08 +0200

    Initial Commit

---
+++ Tokyo/TokyoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TokyoApp.swift
+//  Tokyo
+//
+//  Created by Vikram on 12/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct TokyoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
